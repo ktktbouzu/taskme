1c970239a396dbca6908cdaf969d38277444c01e
aldrin <souma@LAPTOP-0QQC35GO>
2023-12-16 14:54:22 +0800

    TSK-005: Add click highlight to calendar cells

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {
   Container,
   Row,
-  Col
+  Col,
+  Modal
 } from 'react-bootstrap';
 
 import List from '../List';
+import Calendar from '../Calendar';
 import './style.css';
 
 import type {
@@ -24,31 +26,42 @@ const boardDefaultProps = {
 
 const Board = (props: BoardProps) => {
   return (
-    <Container fluid className="board">
-      <Row>
-        <Col className="board__header">
-          <h1>{ props.title }</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {/** Calendar */}
-        </Col>
-      </Row>
-      <Row>
-        { props.lists.map((list: ListConfig) => (
-          <Col 
-            xs={12} 
-            sm={6} 
-            lg={3} 
-            key={`${list.id}-container`}
-            data-testid="list-child"
-          >
-            <List listConfig={list} />
+    <>
+      <Container fluid className="board">
+        <Row>
+          <Col className="board__header">
+            <h1>{props.title}</h1>
           </Col>
-        )) }
-      </Row>
-    </Container>
+        </Row>
+        <Row>
+          <Col>
+            {/** Calendar */}
+            <Calendar />
+          </Col>
+        </Row>
+        <Row>
+          {props.lists.map((list: ListConfig) => (
+            <Col
+              xs={12}
+              sm={6}
+              lg={3}
+              key={`${list.id}-container`}
+              data-testid="list-child"
+            >
+              <List listConfig={list} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <Modal show={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Calendar />
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
 
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -5,34 +5,52 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
+import { has } from 'lodash';
+
+import CalendarCell from './CalendarCell';
 
 import './style.css';
 
+type DateConfig = {
+  type: string,
+  value: string,
+  displayVal: string|number
+};
+
 const getDates = (year: number, month: number) => {
-  const stringDate = `${year}-${month}-01`;
-  const dateStart = moment(stringDate);
+  const dateStart = moment().year(year).month(month - 1).date(1);
   const endOfMonth = moment(dateStart).endOf('month');
   const prevMonth = moment(dateStart).subtract(1, 'months').endOf('month');
-  console.log(prevMonth.format('YYYY-MM-DD'));
+  const nextMonth = moment(dateStart).add(1, 'months');
 
   const currMonthDates = Array.from(
     { length: endOfMonth.date() }, 
-    (elem, index) => ({ type: 'curr', value: index + 1 }) 
+    (elem, index) => ({ 
+      type: 'curr', 
+      value: `${dateStart.year()}-${dateStart.month() + 1}-${index+1}`,
+      displayVal: index + 1
+    }) 
   );
 
   const prevPadding = Array.from(
     { length: dateStart.day() }, 
-    (elem, index) => ({
-      type: 'prev',
-      value: prevMonth.date() - (dateStart.day() - (index + 1))
-    })
+    (elem, index) =>  {
+      const dateVal = prevMonth.date() - (dateStart.day() - (index + 1));
+      
+      return {
+        type: 'prev',
+        value: `${prevMonth.year()}-${prevMonth.month() + 1}-${dateVal}`,
+        displayVal: dateVal
+      };
+    }
   );
 
   const nextPadding = Array.from(
     { length: 6 - endOfMonth.day()  }, 
     (elem, index) => ({
       type: 'next',
-      value: index + 1
+      value: `${nextMonth.year()}-${nextMonth.month() + 1}-${index + 1}`,
+      displayVal: index + 1
     })
   );
 
@@ -43,18 +61,21 @@ const getDates = (year: number, month: number) => {
   ];
 };
 
-const getToday = () => {
-  const today = moment();
+const getDateObj = (dateString: string) => {
+  const dateObj = moment(dateString);
 
   return {
-    year: today.year(),
-    month: today.month() + 1,
-    date: today.date(),
+    year: dateObj.year(),
+    month: dateObj.month() + 1,
+    monthString: dateObj.format('MMMM'),
+    date: dateObj.date(),
   };
 }
 
 const Calendar = () => {
-  const [ currDate, setCurrDate ] = useState(getToday());
+  const [ currDate, setCurrDate ] = useState(moment().format('YYYY-MM-DD'));
+  const [ selected, setSelected ] = useState({});
+  const [ today ] = useState(moment().format('YYYY-MM-DD'));
 
   const days = [
     'SUN',
@@ -65,14 +86,41 @@ const Calendar = () => {
     'FRI',
     'SAT'
   ];
+  
+  const currDateObj = getDateObj(currDate);
+  const dates = getDates(currDateObj.year, currDateObj.month);
+  
+  const getPrevious = () => {
+    setCurrDate(moment(currDate).subtract(1, 'month').format('YYYY-MM-DD'));
+  }
 
-  const dates = getDates(currDate.year, currDate.month);
-  console.log(dates);
+  const getNext = () => {
+    setCurrDate(moment(currDate).add(1, 'month').format('YYYY-MM-DD'));
+  }
+
+  const onCellClick = (value: any) => {
+    let currSelected:Record<string, boolean> = { ...selected };
+
+    if (has(currSelected, value)) {
+      delete currSelected[value];
+    } else {
+      currSelected =  { ...currSelected, [value]: true };      
+    }
+
+    setSelected(currSelected);
+  }
+  
   return (
     <Container fluid className="calendar">
       <Row>
         <Col className="calendar__headerYear">
-          <h4>{ currDate.year }</h4>
+          <button onClick={getPrevious}>
+            <i className="fa-solid fa-chevron-left"></i>
+          </button>
+          <h4>{ `${currDateObj.monthString} ${currDateObj.year}` }</h4>
+          <button onClick={getNext}>
+            <i className="fa-solid fa-chevron-right"></i>
+          </button>
         </Col>
       </Row>
       <Row>
@@ -83,19 +131,20 @@ const Calendar = () => {
         )) }
       </Row>
       <Row className="calendar__datesRow">
-        { dates.map((dateConfig, index) => {
-            let className = 'calendar__datesCell';
-
-            if (dateConfig.type === 'prev' || dateConfig.type === 'next') {
-              className = `${className} calendar__datesCell--muted`;
-            }
-
-            return (
-              <Col className={className} key={index}>
-                { dateConfig.value }
-              </Col>
-            )
-        }) }
+        { dates.map((dateConfig:DateConfig, index: number) => {
+          return (
+            <CalendarCell 
+              muted={!!(dateConfig.type === 'prev' || dateConfig.type === 'next')} 
+              key={index}
+              displayVal={dateConfig.displayVal}
+              dateValue={dateConfig.value}
+              today={dateConfig.value === today}
+              isSelected={has(selected, dateConfig.value)}
+              onCellClick={onCellClick}
+            />
+          );
+        }) 
+        }
         
       </Row>
     </Container>    
